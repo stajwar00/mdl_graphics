709649893f4bcc0ea6dba87eadbe3ca50e99c1a0
Sunan Tajwar <sunantajwar@Sunans-MBP.home>
2020-05-03 01:22:16 -0400

    testing finally works

--- my_main.c
+++ my_main.c
@@ -1,36 +1,26 @@
 /*========== my_main.c ==========
-
   This is the only file you need to modify in order
   to get a working mdl project (for now).
-
   my_main.c will serve as the interpreter for mdl.
   When an mdl script goes through a lexer and parser,
   the resulting operations will be in the array op[].
-
   Your job is to go through each entry in op and perform
   the required action from the list below:
-
   push: push a new origin matrix onto the origin stack
-
   pop: remove the top matrix on the origin stack
-
   move/scale/rotate: create a transformation matrix
                      based on the provided values, then
                      multiply the current top of the
                      origins stack by it.
-
   box/sphere/torus: create a solid object based on the
                     provided values. Store that in a
                     temporary matrix, multiply it by the
                     current top of the origins stack, then
                     call draw_polygons.
-
   line: create a line based on the provided values. Store
         that in a temporary matrix, multiply it by the
         current top of the origins stack, then call draw_lines.
-
   save: call save_extension with the provided filename
-
   display: view the screen
   =========================*/
 
@@ -73,7 +63,7 @@ void my_main() {
   light[LOCATION][1] = 0.75;
   light[LOCATION][2] = 1;
 
-  light[COLOR][RED] = 0;
+  light[COLOR][RED] = 255;
   light[COLOR][GREEN] = 255;
   light[COLOR][BLUE] = 255;
 
@@ -100,8 +90,11 @@ void my_main() {
   struct constants *reflect;
   reflect = &white;
 
+  color c = {100, 100, 100};
+
   systems = new_stack();
-  tmp = new_matrix(4, 1000);
+  polygons = new_matrix(4, 1000);
+  edges = new_matrix(4, 1000);
   clear_screen( t );
   clear_zbuffer(zb);
   g.red = 0;
@@ -111,20 +104,16 @@ void my_main() {
   print_symtab();
   for (i=0;i<lastop;i++) {
 
-    //printf("%d: ",i);
-
-    //printf("\n");
-
     switch (op[i].opcode)
         {
-        case LIGHT:
 
+        case LIGHT:
           light[COLOR][RED] = op[i].op.light.c[RED];
           light[COLOR][GREEN] = op[i].op.light.c[GREEN];
           light[COLOR][BLUE] = op[i].op.light.c[BLUE];
-
           break;
-        case AMBIENT:
+
+        case AMBIENT:         
           ambient.red = op[i].op.ambient.c[RED];
           ambient.green = op[i].op.ambient.c[GREEN];
           ambient.blue = op[i].op.ambient.c[BLUE];
@@ -177,7 +166,7 @@ void my_main() {
         case LINE:
           add_edge(edges, op[i].op.line.p0[0], op[i].op.line.p0[1], op[i].op.line.p0[2], op[i].op.line.p1[0], op[i].op.line.p1[1], op[i].op.line.p1[2]);
           matrix_mult(peek(systems),edges);
-          draw_lines(edges, t, zb, g);
+          draw_lines(edges, t, zb, c);
           edges->lastcol = 0;
           break;
 
@@ -193,7 +182,7 @@ void my_main() {
           tmp = make_translate(op[i].op.move.d[0], op[i].op.move.d[1], op[i].op.move.d[2]);
           matrix_mult(peek(systems),tmp);
           copy_matrix(tmp, peek(systems));
-           free_matrix(tmp);
+          free_matrix(tmp);
           break;
 
         case SCALE:
@@ -212,51 +201,59 @@ void my_main() {
           copy_matrix(tmp, peek(systems));
           free_matrix(tmp);
           break;
-          
+
         case BASENAME:
           printf("WIP: Basename\n");
           break;
+
         case SAVE_KNOBS:
           printf("WIP: Save knobs\n");
           break;
+
         case TWEEN:
           printf("WIP: Tween\n");
           break;
+
         case FRAMES:
           printf("WIP: Frames\n");
           break;
+
         case VARY:
           printf("WIP: Vary\n");
           break;
-        case PUSH:
-          
+
+        case PUSH:         
           push(systems);
-
           break;
-        case POP:
-          
+
+        case POP:         
           pop(systems);
-
           break;
+
         case GENERATE_RAYFILES:
           printf("WIP: Generate rayfiles\n");
           break;
+
         case SAVE:
           save_extension(t, op[i].op.save.p->name);
-
           break;
+
         case SHADING:
           printf("WIP: Shading\n");
           break;
+
         case SETKNOBS:
           printf("WIP: Set knobs\n");
           break;
+
         case FOCAL:
           printf("WIP: Focal\n");
           break;
+
         case DISPLAY:
           display(t);
           break;
+
         }
   
     //printf("\n");
